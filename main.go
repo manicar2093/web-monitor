ef5e30b849fbe40cbe1a3eba8b4f0c86dc749f68
Manuel Carbajal <manicar2093@gmail.com>
2021-05-25 18:33:19 -0500

    Se realiza cambio de la estructura del proyecto

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"embed"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/manicar2093/web-monitor/internal/controllers"
+	"github.com/manicar2093/web-monitor/internal/services"
+)
+
+//go:embed templates/*
+var tpl embed.FS
+var controller controllers.TemplateController
+var templateService services.TemplateService
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", controller.IndexPage)
+
+	log.Fatal("Error al iniciar servidor: ", http.ListenAndServe(":7890", router))
+
+}
+
+func init() {
+	templateService = services.NewTemplateService(&tpl)
+	controller = controllers.NewTemplateController(templateService)
+}
