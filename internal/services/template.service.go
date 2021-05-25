23b53596bec9d07536700de2f5fc3d5a90468b8a
Manuel Carbajal <manicar2093@gmail.com>
2021-05-25 18:21:23 -0500

    Se agregan pruebas con los templates

--- internal/services/template.service.go
+++ internal/services/template.service.go
@@ -2,12 +2,28 @@ package services
 
 import (
 	"embed"
+	"fmt"
+	"html/template"
 	"net/http"
 )
+
+var tplFiles *template.Template
 
 type TemplateService interface {
 	Render(w http.ResponseWriter, templateName string, content interface{}) error
 }
 
-// go:embed ../../templates
-var templates embed.FS
+type TemplateServiceImpl struct{}
+
+func NewTemplateService(files *embed.FS) TemplateService {
+	tplFiles = template.Must(template.ParseFS(files, "templates/*.html"))
+	return &TemplateServiceImpl{}
+}
+
+func (t TemplateServiceImpl) Render(w http.ResponseWriter, templateName string, content interface{}) error {
+	err := tplFiles.ExecuteTemplate(w, fmt.Sprintf("%s.html", templateName), content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
