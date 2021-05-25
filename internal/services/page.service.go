e884aa13c0a7c7e6c3bcc1b9715501b2ce662ea8
Manuel Carbajal <manicar2093@gmail.com>
2021-05-24 21:15:58 -0500

    Se agregan servicios para phrases y pages

--- internal/services/page.service.go
+++ internal/services/page.service.go
@@ -1,11 +1,52 @@
 package services
 
-import "github.com/manicar2093/web-monitor/internal/entities"
+import (
+	"github.com/google/uuid"
+	"github.com/manicar2093/web-monitor/internal/dao"
+	"github.com/manicar2093/web-monitor/internal/entities"
+)
 
 type PageService interface {
 	GetAllPages() ([]entities.Page, error)
-	DeletePage(pageID int32) error
+	DeletePage(pageID string) error
 	AddPage(page entities.Page) (entities.Page, error)
 	EditPage(data entities.Page) error
 	PageExists(url string) (bool, error)
+}
+
+type PageServiceImpl struct {
+	pageDao dao.PageDao
+}
+
+func NewPageDao(pageDao dao.PageDao) PageService {
+	return &PageServiceImpl{pageDao: pageDao}
+}
+
+func (p PageServiceImpl) GetAllPages() ([]entities.Page, error) {
+	return p.pageDao.GetAllPages()
+}
+
+func (p PageServiceImpl) DeletePage(pageID string) error {
+	return p.pageDao.Delete(pageID)
+}
+
+func (p PageServiceImpl) AddPage(page entities.Page) (entities.Page, error) {
+	page.ID = uuid.NewString()
+	err := p.pageDao.Save(page)
+	return page, err
+}
+
+func (p PageServiceImpl) EditPage(data entities.Page) error {
+	return p.pageDao.Update(&data)
+}
+
+func (p PageServiceImpl) PageExists(url string) (bool, error) {
+	_, err := p.pageDao.FindPageByURL(url)
+	if err != nil {
+		if err == dao.ErrNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
--- internal/services/phrase.service.go
+++ internal/services/phrase.service.go
@@ -1,12 +1,39 @@
 package services
 
 import (
+	"github.com/google/uuid"
+	"github.com/manicar2093/web-monitor/internal/dao"
 	"github.com/manicar2093/web-monitor/internal/entities"
 )
 
 type PhraseService interface {
 	GetAllPhrases() ([]entities.Phrase, error)
-	DeletePhrase(phraseID int32) error
+	DeletePhrase(phraseID string) error
 	AddPhrase(phrase entities.Phrase) error
 	EditPhrase(data entities.Phrase) error
+}
+
+type PhraseServiceImpl struct {
+	phraseDao dao.PhraseDao
+}
+
+func NewPhraseService(phraseDao dao.PhraseDao) PhraseService {
+	return &PhraseServiceImpl{phraseDao: phraseDao}
+}
+
+func (p PhraseServiceImpl) GetAllPhrases() ([]entities.Phrase, error) {
+	return p.phraseDao.GetAllPhrases()
+}
+
+func (p PhraseServiceImpl) DeletePhrase(phraseID string) error {
+	return p.phraseDao.Delete(phraseID)
+}
+
+func (p PhraseServiceImpl) AddPhrase(phrase entities.Phrase) error {
+	phrase.ID = uuid.NewString()
+	return p.phraseDao.Save(phrase)
+}
+
+func (p PhraseServiceImpl) EditPhrase(data entities.Phrase) error {
+	return p.phraseDao.Update(&data)
 }
