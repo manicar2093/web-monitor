cd8ed06a5e54ce71ec231fada60000082793aeaa
Manuel Carbajal <manicar2093@gmail.com>
2021-05-30 22:43:09 -0500

    Se implementa servicio de validación para las paginas

---
+++ services/validator.service.go
@@ -0,0 +1,81 @@
+package services
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"github.com/manicar2093/web-monitor/dao"
+	"github.com/manicar2093/web-monitor/models"
+)
+
+type Notification struct {
+	PageID     string `json:"pageID"`
+	Error      string `json:"error"`
+	StatusCode int    `json:"status_code, omitempty"`
+	Cause      string `json:"cause"`
+}
+
+type ValidatorService interface {
+	// ValidatePages valida las paginas. Al haber error realiza el panic
+	Start()
+}
+
+type ValidatorServiceImpl struct {
+	Minutes   int
+	pagesDao  dao.PageDao
+	observers []models.Observer
+	_cron     *gocron.Scheduler
+	client    *http.Client
+}
+
+func NewValidatorService(minutes int, pagesDao dao.PageDao, observers ...models.Observer) ValidatorService {
+	c := &http.Client{
+		Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+	}
+	v := &ValidatorServiceImpl{minutes, pagesDao, observers, gocron.NewScheduler(time.UTC), c}
+	v.Start()
+	return v
+}
+
+// ValidatePages valida las paginas. Al haber error realiza el panic
+func (v ValidatorServiceImpl) Start() {
+	v._cron.Every(v.Minutes).Seconds().Tag("validator").Do(v.validatePages)
+	v._cron.StartAsync()
+}
+
+func (v ValidatorServiceImpl) validatePages() {
+	log.Println("Comienza la validación")
+	pages, err := v.pagesDao.GetAllPages()
+	if err != nil {
+		panic(err)
+	}
+	log.Println(pages)
+	for _, d := range pages {
+		log.Println("validando", d.Name)
+		res, err := v.client.Get(d.URL)
+		log.Println(res)
+
+		if err != nil || res.StatusCode != http.StatusOK {
+			log.Println("Error ", err, " en", d.Name)
+			v.notifyAll(&Notification{PageID: d.ID, Error: err.Error(), Cause: "Error on client :/"})
+			d.Status = false
+			v.pagesDao.Update(&d)
+			continue
+		}
+
+		d.Status = true
+		v.pagesDao.Update(&d)
+
+	}
+
+	log.Println("Termina la validación de paginas")
+}
+
+func (v ValidatorServiceImpl) notifyAll(data interface{}) {
+	for _, d := range v.observers {
+		d.Notify(data)
+	}
+}
