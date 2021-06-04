35df23b5bb50410157897abee2822f8d3d926d6c
Manuel Carbajal <manicar2093@gmail.com>
2021-06-03 21:36:54 -0500

    Se continua con la vista de la aplicación

--- static/js/components/registry.js
+++ static/js/components/registry.js
@@ -1,36 +1,52 @@
 Vue.component("registry", {
-    props: ["type", "page_name", "url"],
+    props: ["type", "status", "data", "show_options"],
     delimiters: ['[[', ']]'],
     template: /*html*/ `
     <div :class="generalClasses">
-        <div v-if="verifyType">
+        <div v-if="status == 'down'">
             <div class="registry__time">
                 <p>00:00:00</p>
             </div>
         </div>
-        <div class="registry__page_name"><a :href="url" target="_blank">[[ page_name ]]</a></div>
-        <div v-if="verifyType">
-            <div class="registry__actions">
-                <button @click="validatePage" class="button is-info" title="Revisamos el status de la página">Actualizar</button>
+        
+        <div class="registry__page_name">
+            <div v-if="type == 'phrase'" >
+                [[ data.phrase ]]
             </div>
+
+            <div v-else-if="type == 'page'">
+                <a :href="data.url" target="_blank">[[ data.name ]]</a>
+            </div>
+            
+        </div>
+        <div class="registry__actions">
+            <button v-if="type == 'page' && status == 'down'" @click="validatePage" class="button is-info" title="Revisamos el status de la página">Actualizar</button>
+            <div v-if="show_options">
+                <button class="button is-danger">Eliminar</button>
+                <button class="button is-warning">Editar</button>
+            </div>
+            
         </div>
     </div>
     `,
     methods: {
         validatePage() {
             this.$emit("verifyPage", this.url)
+        },
+        delete( ) {
+            this.$emit("delete", this.data)
+        },
+        update() {
+            this.$emit("update", this.data)
         }
     },
     computed: {
         generalClasses() {
-            if(!this.type) {
+            if(!this.status) {
                 return "registry"
             }
-            let registryType = `registry--${this.type}`
+            let registryType = `registry--${this.status}`
             return `registry ${registryType}`
         },
-        verifyType() {
-            return this.type == 'down' || !this.type
-        }
     }
 })
