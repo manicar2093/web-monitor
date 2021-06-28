3699336d1c6eabe35335ba820ad3833a2caa8ac4
Manuel Carbajal <manicar2093@gmail.com>
2021-06-28 00:03:30 -0500

    feat: validator test created

--- services/validator.service_test.go
+++ services/validator.service_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/manicar2093/web-monitor/entities"
 	"github.com/manicar2093/web-monitor/mocks"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidatePage(t *testing.T) {
@@ -23,12 +24,18 @@ func TestValidatePage(t *testing.T) {
 
 	client.On("Get", page.URL).Return(&http.Response{
 		StatusCode: http.StatusNotFound,
-	})
+	}, nil)
 	pageDao.On("Update", &page).Return(nil)
 
 	validator := NewValidatorService(&pageDao, &client)
 
 	n, _ := validator.ValidatePage(&page, true)
 
-	t.Log(n)
+	client.AssertExpectations(t)
+	pageDao.AssertExpectations(t)
+
+	assert.Equal(t, n.Page.IsWorking, false, "should not working")
+	assert.Equal(t, n.Page.Recovered, false, "should not recovered")
+	assert.Equal(t, n.Page.Code, http.StatusNotFound, "should return a 404 code")
+
 }
