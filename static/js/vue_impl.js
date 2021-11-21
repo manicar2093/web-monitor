1253548bb3779c2bbd03c74244ef1db211de90e7
Manuel Carbajal <manicar2093@gmail.com>
2021-11-20 23:57:35 -0600

    fix: Add some js fixes on vue_impl.js
    - Add id field on js form
    - Remove interval on pages validation

--- static/js/vue_impl.js
+++ static/js/vue_impl.js
@@ -156,11 +156,13 @@ const app = new Vue({
             }
             const forms = {
                 registerPageForm: {
+                    id: null,
                     name: '',
                     url: '',
                     status: ''
                 },
                 registerPhraseForm: {
+                    id: null,
                     phrase: ''
                 }
             }
@@ -193,6 +195,8 @@ const app = new Vue({
                 this.notifications_accepted = false
                 this.appClosed = true
             }
+
+            clearInterval(this.validationInterval)
         },
         async deletePage(data) {
             const onError = () => alert("ERROR AL BORRAR PAGINA.\nIntenta nuevamente.")
@@ -308,8 +312,6 @@ const app = new Vue({
 
         this.evtSource = new EventSource("/sse/sse-validator");
         this.evtSource.onmessage = this.sseHandler
-        // this.evtSource.onopen = (e) => console.log("Connected to SSE")
-        // this.evtSource.onerror = (e) => console.error(e)
         this.validationInterval = setInterval(this.setIntervalHandler, 120000)
     }
 })
